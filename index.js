d034fa69a99bed462e991d523d938a8b1a803efb
Ajiteru Dolapo <83882048+Dol-Desh@users.noreply.github.com>
2023-11-06 18:57:17 +0100

    fixed and modals

---
+++ index.js
@@ -0,0 +1,23 @@
+function setupModal(modalId, openModalId, closeModalId) {
+  const modal = document.getElementById(modalId);
+  const openModal = document.getElementById(openModalId);
+  const closeModal = document.getElementById(closeModalId);
+
+  if (modal && openModal && closeModal) {
+    openModal.addEventListener("click", () => {
+      modal.showModal();
+    });
+
+    closeModal.addEventListener("click", () => {
+      modal.close();
+    });
+  }
+}
+
+setupModal("menu-modal", "hamburger-menu", "menu-modal-close");
+setupModal("search-modal", "search-menu", "search-modal-close");
+
+window.addEventListener("DOMContentLoaded", function () {
+  const input = document.getElementById("search-input");
+  input.blur();
+});
